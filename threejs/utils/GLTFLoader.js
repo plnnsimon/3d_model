51eb3e10557e4ade2203960a7c4927b65855225a
Олександр Пальонний <s2567k@gmail.com>
2023-01-16 21:48:00 +0200

    updates

--- threejs/utils/GLTFLoader.js
+++ threejs/utils/GLTFLoader.js
@@ -4,7 +4,7 @@ import { DRACOLoader } from 'https://unpkg.com/three@0.148.0/examples/jsm/loader
 
 // const manager = new LoadingManager();
 const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('/draco/')
+dracoLoader.setDecoderPath('/static/draco/')
 dracoLoader.setDecoderConfig({ type: 'js' })
 
 export default class ModelLoader {
